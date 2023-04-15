d6e2e522d86c168229ebde24e37c7c4f536bdfc8
Ariel <dalessandro.ariel@gmail.com>
2023-04-15 10:47:50 +0200

    Fixes (#5)
    * fix(visual select): visual selection was not selecting as expected
    
    * fix(route_inff) treesitter.query Not picking the global change for this, support old nvim version

--- lua/laravel/utils.lua
+++ lua/laravel/utils.lua
@@ -15,9 +15,24 @@ function utils.notify(funname, opts)
 end
 
 utils.get_visual_selection = function()
-  local s_start = vim.fn.getpos "'<"
-  local s_end = vim.fn.getpos "'>"
-  return vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+  local sel = utils.get_vsel()
+  return vim.api.nvim_buf_get_lines(sel.bufnr, sel.pos[1] - 1, sel.pos[3], false)
+end
+
+utils.get_vsel = function()
+  local bufnr = vim.api.nvim_win_get_buf(0)
+  local start = vim.fn.getpos "v" -- [bufnum, lnum, col, off]
+  local _end = vim.fn.getpos "." -- [bufnum, lnum, col, off]
+  if start[2] > _end[2] then
+    local x = _end
+    _end = start
+    start = x
+  end
+  return {
+    bufnr = bufnr,
+    mode = vim.fn.mode(),
+    pos = { start[2], start[3], _end[2], _end[3] },
+  }
 end
 
 return utils

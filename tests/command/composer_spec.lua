2656bb146514dc8e9715b738a962c5de7e46832b
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-10-10 20:35:38 +0200

    clean test

--- tests/command/composer_spec.lua
+++ tests/command/composer_spec.lua
@@ -1,17 +1,17 @@
+local spy = require "luassert.spy"
+
 describe("Composer user command", function()
-  local spy = require('luassert.spy')
-
   local s = spy.new(function() end)
-  package.loaded['laravel.run'] = s
-  require('laravel.user_commands.composer').setup()
+  package.loaded["laravel.run"] = s
+  require("laravel.user_commands.composer").setup()
 
-  it('require formats the parameter', function()
+  it("require formats the parameter", function()
     vim.cmd [[Composer require laravel/folio]]
-    assert.spy(s).was.called_with('composer', { "require", "laravel/folio" }, {})
+    assert.spy(s).was.called_with("composer", { "require", "laravel/folio" }, {})
   end)
 
-  it('passes the arguments to the install', function()
+  it("passes the arguments to the install", function()
     vim.cmd [[Composer install --options]]
-    assert.spy(s).was.called_with('composer', { "install", "--options" }, {})
+    assert.spy(s).was.called_with("composer", { "install", "--options" }, {})
   end)
 end)

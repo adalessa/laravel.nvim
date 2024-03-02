161f940a8d350470384376d494817f5a111ee1f2
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2024-03-02 18:21:10 +0100

    feature: add user commands

--- lua/laravel/config/user_commands.lua
+++ lua/laravel/config/user_commands.lua
@@ -5,4 +5,20 @@ return {
       desc = "Re-creates the db and seed's it",
     },
   },
+  npm = {
+    build = {
+      cmd = { "run", "build" },
+      desc = "Builds the javascript assets",
+    },
+    dev = {
+      cmd = { "run", "dev" },
+      desc = "Builds the javascript assets",
+    },
+  },
+  composer = {
+    autoload = {
+      cmd = { "dump-autoload" },
+      desc = "Dumps the composer autoload",
+    },
+  },
 }

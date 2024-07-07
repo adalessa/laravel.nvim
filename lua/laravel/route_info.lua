ba02246fb49b25ca6e2a9ca9b5064892e2ee0b6d
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2024-07-07 21:35:38 +0200

    update with the new container style

--- lua/laravel/route_info.lua
+++ lua/laravel/route_info.lua
@@ -1,8 +1,7 @@
-local routes = require "laravel.routes"
-local config = require "laravel.config"
+local app = require("laravel.app")
 
 local get_node_text = vim.treesitter.get_node_text
-local options = config.options.features.route_info
+local options = app("options"):get().features.route_info
 
 local function is_same_class(action, class)
   return string.sub(action, 1, string.len(class)) == class
@@ -12,14 +11,14 @@ local function generate_virtual_text_options(route)
   if options.position == "right" then
     return {
       virt_text = {
-        { "[", "comment" },
-        { "Method: ", "comment" },
-        { vim.fn.join(route.methods, "|"), "@enum" },
-        { " Uri: ", "comment" },
-        { route.uri, "@enum" },
-        { " Middleware: ", "comment" },
+        { "[",                                               "comment" },
+        { "Method: ",                                        "comment" },
+        { vim.fn.join(route.methods, "|"),                   "@enum" },
+        { " Uri: ",                                          "comment" },
+        { route.uri,                                         "@enum" },
+        { " Middleware: ",                                   "comment" },
         { vim.fn.join(route.middlewares or { "None" }, ","), "@enum" },
-        { "]", "comment" },
+        { "]",                                               "comment" },
       },
     }
   end
@@ -27,15 +26,15 @@ local function generate_virtual_text_options(route)
     return {
       virt_lines = {
         {
-          { "    ", "" },
-          { "[", "comment" },
-          { "Method: ", "comment" },
-          { vim.fn.join(route.methods, "|"), "@enum" },
-          { " Uri: ", "comment" },
-          { route.uri, "@enum" },
-          { " Middleware: ", "comment" },
+          { "    ",                                            "" },
+          { "[",                                               "comment" },
+          { "Method: ",                                        "comment" },
+          { vim.fn.join(route.methods, "|"),                   "@enum" },
+          { " Uri: ",                                          "comment" },
+          { route.uri,                                         "@enum" },
+          { " Middleware: ",                                   "comment" },
           { vim.fn.join(route.middlewares or { "None" }, ","), "@enum" },
-          { "]", "comment" },
+          { "]",                                               "comment" },
         },
       },
       virt_lines_above = true,
@@ -45,96 +44,88 @@ end
 
 return function(event)
   local bufnr = event.buf
-  local namespace = vim.api.nvim_create_namespace "laravel.routes"
+  local namespace = vim.api.nvim_create_namespace("laravel.routes")
 
   -- clean namespace
   vim.api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
   vim.diagnostic.reset(namespace, bufnr)
 
   local setRouteInfo = function()
-    local php_parser = vim.treesitter.get_parser(bufnr, "php")
-    local tree = php_parser:parse()[1]
-    if tree == nil then
-      error("Could not retrieve syntx tree", vim.log.levels.ERROR)
-    end
-
-    local query = vim.treesitter.query.get("php", "php_class")
-    if not query then
-      error("Could not get treesitter query", vim.log.levels.ERROR)
-    end
-    local class, class_namespace, methods, visibilities = "", "", {}, {}
-    local class_pos = 0
-
-    for id, node in query:iter_captures(tree:root(), bufnr) do
-      if query.captures[id] == "class" then
-        class = get_node_text(node, bufnr)
-        class_pos = node:start()
-      elseif query.captures[id] == "namespace" then
-        class_namespace = get_node_text(node, bufnr)
-      elseif query.captures[id] == "method" then
-        table.insert(methods, {
-          pos = node:start(),
-          name = get_node_text(node, bufnr),
-        })
-      elseif query.captures[id] == "visibility" then
-        table.insert(visibilities, get_node_text(node, bufnr))
+    app("routes"):get(function(routes)
+      local php_parser = vim.treesitter.get_parser(bufnr, "php")
+      local tree = php_parser:parse()[1]
+      if tree == nil then
+        error("Could not retrieve syntx tree", vim.log.levels.ERROR)
       end
-    end
 
-    local class_methods = {}
-
-    local full_class = string.format("%s\\%s", class_namespace, class)
-    for idx, method in ipairs(methods) do
-      if visibilities[idx] == "public" then
-        table.insert(class_methods, {
-          full = string.format("%s\\%s@%s", class_namespace, class, method.name),
-          name = method.name,
-          pos = method.pos,
-        })
+      local query = vim.treesitter.query.get("php", "php_class")
+      if not query then
+        error("Could not get treesitter query", vim.log.levels.ERROR)
       end
-    end
+      local class, class_namespace, methods, visibilities = "", "", {}, {}
+      local class_pos = 0
 
-    local errors = {}
-    for _, route in pairs(routes.list) do
-      local found = false
-      for _, method in pairs(class_methods) do
-        local action_full = route.action
-        if vim.fn.split(route.action, "@")[2] == nil then
-          action_full = action_full .. "@__invoke"
-        end
-        if action_full == method.full then
-          vim.api.nvim_buf_set_extmark(bufnr, namespace, method.pos, 0, generate_virtual_text_options(route))
-          found = true
+      for id, node in query:iter_captures(tree:root(), bufnr) do
+        if query.captures[id] == "class" then
+          class = get_node_text(node, bufnr)
+          class_pos = node:start()
+        elseif query.captures[id] == "namespace" then
+          class_namespace = get_node_text(node, bufnr)
+        elseif query.captures[id] == "method" then
+          table.insert(methods, {
+            pos = node:start(),
+            name = get_node_text(node, bufnr),
+          })
+        elseif query.captures[id] == "visibility" then
+          table.insert(visibilities, get_node_text(node, bufnr))
         end
       end
 
-      if is_same_class(route.action, full_class) and not found then
-        table.insert(errors, {
-          lnum = class_pos,
-          col = 0,
-          message = string.format(
-            "missing method %s [Method: %s, URI: %s]",
-            vim.fn.split(route.action, "@")[2] or "__invoke",
-            vim.fn.join(route.methods, "|"),
-            route.uri
-          ),
-        })
+      local class_methods = {}
+
+      local full_class = string.format("%s\\%s", class_namespace, class)
+      for idx, method in ipairs(methods) do
+        if visibilities[idx] == "public" then
+          table.insert(class_methods, {
+            full = string.format("%s\\%s@%s", class_namespace, class, method.name),
+            name = method.name,
+            pos = method.pos,
+          })
+        end
       end
-    end
 
-    if #errors > 0 then
-      vim.diagnostic.set(namespace, bufnr, errors)
-    end
-  end
+      local errors = {}
+      for _, route in routes:enumerate() do
+        local found = false
+        for _, method in pairs(class_methods) do
+          local action_full = route.action
+          if vim.fn.split(route.action, "@")[2] == nil then
+            action_full = action_full .. "@__invoke"
+          end
+          if action_full == method.full then
+            vim.api.nvim_buf_set_extmark(bufnr, namespace, method.pos, 0, generate_virtual_text_options(route))
+            found = true
+          end
+        end
 
-  -- WARN: clean should be easier to resolve
-  if #routes.list == 0 then
-    routes.asyncLoad(function(result)
-      if result:successful() then
-        setRouteInfo()
+        if is_same_class(route.action, full_class) and not found then
+          table.insert(errors, {
+            lnum = class_pos,
+            col = 0,
+            message = string.format(
+              "missing method %s [Method: %s, URI: %s]",
+              vim.fn.split(route.action, "@")[2] or "__invoke",
+              vim.fn.join(route.methods, "|"),
+              route.uri
+            ),
+          })
+        end
+      end
+      if #errors > 0 then
+        vim.diagnostic.set(namespace, bufnr, errors)
       end
     end)
-  else
-    setRouteInfo()
   end
+
+  setRouteInfo()
 end

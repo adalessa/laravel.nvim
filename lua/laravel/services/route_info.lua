7cbfaf12892b42e76805f1c4d2c9bb5389a46407
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2024-07-29 22:39:53 +0200

    fix route info from iterable

--- lua/laravel/services/route_info.lua
+++ lua/laravel/services/route_info.lua
@@ -32,7 +32,7 @@ function route_info:handle(bufnr)
     end
     self.routes:get(vim.schedule_wrap(function(routes)
       local errors = {}
-      for _, route in routes:enumerate() do
+      for _, route in routes do
         local found = false
         for _, method in pairs(class.methods) do
           local action_full = route.action

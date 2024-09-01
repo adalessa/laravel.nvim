b49f72b9020ce6ccd13c9e8ef79be2e617abbe67
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2024-09-01 08:28:34 +0200

    fix when no type

--- lua/laravel/services/model_info.lua
+++ lua/laravel/services/model_info.lua
@@ -49,7 +49,7 @@ function model_info:handle(bufnr)
               for _, attribute in ipairs(info.attributes) do
                 table.insert(virt_lines, {
                   { "   " .. attribute.name, "@enum" },
-                  { " " .. attribute.type,   "comment" },
+                  attribute.type and { " " .. attribute.type,   "comment" } or nil,
                   attribute.cast and { " -> " .. attribute.cast, "@enum" } or nil,
                 })
               end

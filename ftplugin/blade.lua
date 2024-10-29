39ae9721477e43a5f40c71ed1ec732268b6e5ffc
weirdlooop <92607002+weirdlooop@users.noreply.github.com>
2024-10-29 17:11:39 +0000

    removed redundant keymap as it interferes with f key bindings
    This interferes with plugins that override the f key behaviour such as eyeliner.lua
    
    There isn't any benefit to having the code in there as it doesn't do anything, and it stops the delay when pressing the f key

--- ftplugin/blade.lua
+++ ftplugin/blade.lua
@@ -1,6 +1,7 @@
 --- Here set things for blade
-vim.keymap.set({ "n" }, "lg", function()
-  vim.print("Here needs to find the view if in extends or how")
 
-  vim.cmd("normal! lg")
-end, { buffer = 0 })
+-- vim.keymap.set({ "n" }, "fg", function()
+--   vim.print("Here needs to find the view if in extends or how")
+--
+--   vim.cmd("normal! fg")
+-- end, { buffer = 0 })
--- ftplugin/php.lua
+++ ftplugin/php.lua
@@ -1,7 +1,7 @@
 --- Here set things for php buffers
 
-vim.keymap.set({ "n" }, "lg", function()
-  vim.print("checks if is in a view if not trigger the old lg")
-
-  vim.cmd("normal! lg")
-end, { buffer = 0 })
+-- vim.keymap.set({ "n" }, "fg", function()
+--   vim.print("checks if is in a view if not trigger the old fg")
+--
+--   vim.cmd("normal! fg")
+-- end, { buffer = 0 })

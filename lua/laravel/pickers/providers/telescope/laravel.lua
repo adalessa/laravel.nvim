6ded01650a540d3ab6a209ca245332049357e88e
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2025-09-12 19:55:33 +0200

    fix: forgot to run the picker

--- lua/laravel/pickers/providers/telescope/laravel.lua
+++ lua/laravel/pickers/providers/telescope/laravel.lua
@@ -7,31 +7,33 @@ local action_state = require("telescope.actions.state")
 local laravel_picker = {}
 
 function laravel_picker.run(opts, commands)
-  pickers.new(opts or {}, {
-    prompt_title = "Laravel commands",
-    finder = finders.new_table({
-      results = commands,
-      entry_maker = function(command)
-        return {
-          value = command,
-          display = command.signature,
-          ordinal = command.signature,
-        }
-      end,
-    }),
-    sorter = conf.prefilter_sorter({
-      sorter = conf.generic_sorter(opts or {}),
-    }),
-    attach_mappings = function(_, map)
-      map("i", "<cr>", function(prompt_bufnr)
-        actions.close(prompt_bufnr)
-        local entry = action_state.get_selected_entry()
-        entry.value:handle()
-      end)
+  pickers
+    .new(opts or {}, {
+      prompt_title = "Laravel commands",
+      finder = finders.new_table({
+        results = commands,
+        entry_maker = function(command)
+          return {
+            value = command,
+            display = command.signature,
+            ordinal = command.signature,
+          }
+        end,
+      }),
+      sorter = conf.prefilter_sorter({
+        sorter = conf.generic_sorter(opts or {}),
+      }),
+      attach_mappings = function(_, map)
+        map("i", "<cr>", function(prompt_bufnr)
+          actions.close(prompt_bufnr)
+          local entry = action_state.get_selected_entry()
+          entry.value:handle()
+        end)
 
-      return true
-    end,
-  })
+        return true
+      end,
+    })
+    :find()
 end
 
 return laravel_picker

a78db63740016d2ae55cefd8907cc62d2ef0e200
Jesse Tolj <jesse@objectfactory.io>
2025-12-18 11:49:25 -0500

    fix: typo in resources picker

--- lua/laravel/commands/resources.lua
+++ lua/laravel/commands/resources.lua
@@ -2,11 +2,11 @@ local app = require("laravel.core.app")
 local nio = require("nio")
 
 return {
-  signature = "picker:resoruces",
-  description = "Open the resoruces picker",
+  signature = "picker:resources",
+  description = "Open the resources picker",
   handle = nio.create(function()
     ---@type laravel.managers.pickers_manager
     local pickers = app:make("pickers_manager")
-    pickers:run("resoruces")
+    pickers:run("resources")
   end, 1),
 }

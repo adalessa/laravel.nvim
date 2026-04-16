51d91652e87ff1d24ad3a80eae44388f05b9241c
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2026-04-16 08:43:05 +0200

    refactor pty command

--- lua/laravel/pty_command.lua
+++ lua/laravel/pty_command.lua
@@ -3,10 +3,12 @@ local BaseCommand = require("laravel.base_command")
 local PtyCommand = setmetatable({}, { __index = BaseCommand })
 PtyCommand.__index = PtyCommand
 
-function PtyCommand:addCallback(callback)
-  self.on_output = callback
-
-  return self
+function PtyCommand:onStdout(data)
+  for _, line in ipairs(data) do
+    if line ~= "" then
+      vim.fn.chansend(self.channel_id, line .. "\n")
+    end
+  end
 end
 
 function PtyCommand:execute()
@@ -16,14 +18,7 @@ function PtyCommand:execute()
   self.job_id = vim.fn.jobstart(self.cmd, {
     pty = true,
     on_stdout = function(_, data)
-      for _, line in ipairs(data) do
-        if line ~= "" then
-          vim.fn.chansend(self.channel_id, line .. "\n")
-          if self.on_output then
-            self.on_output(line)
-          end
-        end
-      end
+      self:onStdout(data)
     end,
     on_exit = function(_, exit_code)
       self.exit_code = exit_code

5b2920eec886cb028a08d17e8582632f9056b6ba
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2026-05-02 21:09:42 +0200

    support blik

--- lua/laravel/extensions/completion/blink.lua
+++ lua/laravel/extensions/completion/blink.lua
@@ -1,3 +1,6 @@
+local nio = require("nio")
+local app = require("laravel.core.app")
+
 --- @module 'blink.cmp'
 --- @class blink.cmp.Source
 local source = {}
@@ -18,10 +21,90 @@ end
 
 function source:get_completions(ctx, callback)
   -- could get the line with normal
-  local line = ctx.get_line()
-  local col = ctx.get_cursor()[2]
+  -- local line = ctx.get_line()
+  -- local col = ctx.get_cursor()[2]
   -- check how do I want to identified the completions.
   -- Need to get the hole line and the column
+  local a = ctx.get_bounds("prefix")
+  -- to work the same need the
+
+  local text = a.line:sub(1, a.start_col)
+
+  nio.run(function()
+    local views_completion = require("laravel.extensions.completion.views_completion")
+    if views_completion.shouldComplete(text) then
+      return views_completion.complete(
+        app("laravel.loaders.views_loader"),
+        app("laravel.utils.templates"),
+        {},
+        callback
+      )
+    end
+
+    local inertia_completion = require("laravel.extensions.completion.inertia_completion")
+    if inertia_completion.shouldComplete(text) then
+      return inertia_completion.complete(app("laravel.loaders.inertia_cache_loader"), {}, {}, callback)
+    end
+
+    local configs_completion = require("laravel.extensions.completion.configs_completion")
+    if configs_completion.shouldComplete(text) then
+      return configs_completion.complete(
+        app("laravel.loaders.configs_loader"),
+        app("laravel.utils.templates"),
+        {},
+        callback
+      )
+    end
+
+    local routes_completion = require("laravel.extensions.completion.routes_completion")
+    if routes_completion.shouldComplete(text) then
+      return routes_completion.complete(
+        app("laravel.loaders.routes_cache_loader"),
+        app("laravel.utils.templates"),
+        {},
+        callback
+      )
+    end
+
+    local env_completion = require("laravel.extensions.completion.env_vars_completion")
+    if env_completion.shouldComplete(text) then
+      return env_completion.complete(
+        app("laravel.loaders.environment_variables_cache_loader"),
+        app("laravel.utils.templates"),
+        {},
+        callback
+      )
+    end
+
+    -- local model_completion = require("laravel.extensions.completion.model_completion")
+    -- if model_completion.shouldComplete(text) then
+    --   return model_completion.complete(self.templates, params, callback)
+    -- end
+
+    callback({ items = {} })
+  end)
+
+  -- local items = {}
+
+  -- table.insert(items, {
+  --   label = "laravel",
+  --   kind = require('blink.cmp.types').CompletionItemKind.Text,
+  --   documentation = "A simple function that returns 'blink'",
+  --   -- insertText = "laravel",
+  --   -- textEdit = {
+  --   --   newText = "laravel",
+  --   --   range = {
+  --   --     start = { line = ctx.get_cursor()[1] - 1, character = col - 1 },
+  --   --     ["end"] = { line = ctx.get_cursor()[1] - 1, character = col },
+  --   --   },
+  --   -- },
+  -- })
+  --
+  -- callback({
+  --   items = items,
+  --   is_incomplete_backward = false,
+  --   is_incomplete_forward = false,
+  -- })
   return function() end
 end
 
